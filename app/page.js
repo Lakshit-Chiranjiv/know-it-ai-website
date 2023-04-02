15e552516c933909f5fbe6089a0c23c51a38648b
Lakshit-Chiranjiv <luckyhacker017@gmail.com>
2023-04-02 23:43:45 +0530

    generate summary func

--- app/page.js
+++ app/page.js
@@ -2,13 +2,26 @@
 import Image from 'next/image'
 import { Poppins } from 'next/font/google'
 import logo from '../public/knowitlogo.png'
+import OpenAI from 'openai-api'
 
 import { useState } from 'react'
 
 
 export default function Home() {
 
+  const openai = new OpenAI(process.env.OPENAI_API_KEY)
   const [search, setSearch] = useState('')
+
+  const generateSummary = async (topic) => {
+    const result = await openai.complete({
+      engine: 'davinci',
+      prompt: `Generate a summary for ${topic}.`,
+      maxTokens: 50,
+    });
+    const summary = result.data.choices[0].text;
+    return summary;
+  };
+  
 
   return (
     <main className='bg-slate-600 min-h-screen'>
@@ -21,8 +34,12 @@ export default function Home() {
           className='mb-10'
         />
         <h1 className='text-2xl md:text-3xl lg:text-5xl text-white font-bold'>Search for any topic</h1>
-        <input className='w-3/4 md:w-1/2 lg:w-1/3 h-10 rounded-md text-center outline-none' type='text' placeholder='Search' />
-        <button className='w-3/4 md:w-1/2 lg:w-1/3 h-10 rounded-md bg-slate-500 text-white font-bold active:scale-95'>Know</button>
+        <input className='w-3/4 md:w-1/2 lg:w-1/3 h-10 rounded-md text-center outline-none' type='text' placeholder='Search' onChange={(event) => {
+          setSearch(event.target.value)
+        }}/>
+        <button className='w-3/4 md:w-1/2 lg:w-1/3 h-10 rounded-md bg-slate-500 text-white font-bold active:scale-95' onClick={() => {
+
+        }}>Know</button>
       </div>
     </main>
   )
