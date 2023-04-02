cfb8708f26c45744832103d23389dcbdd2d9bec6
Lakshit-Chiranjiv <luckyhacker017@gmail.com>
2023-04-02 23:16:53 +0530

    input

--- app/page.js
+++ app/page.js
@@ -6,12 +6,17 @@ import logo from '../public/knowitlogo.png'
 export default function Home() {
 
   return (
-    <main className='bg-slate-600 h-screen'>
-      <div className='flex flex-col items-center justify-center h-full'>
+    <main className='bg-slate-600 min-h-screen'>
+      <div className='flex flex-col items-center justify-center gap-12 h-full'>
         <Image
           src={logo}
           alt='Know It Logo'
+          width={400}
+          height={400}
+          className='mb-10'
         />
+        <h1 className='text-2xl md:text-3xl lg:text-5xl text-white font-bold'>Search for any topic</h1>
+        <input className='w-3/4 md:w-1/2 lg:w-1/3 h-10 rounded-md text-center outline-none' type='text' placeholder='Search' />
       </div>
     </main>
   )
