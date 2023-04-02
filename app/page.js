54ec36391a9a94ea8fbdb9eb74b96209cd9d1bec
Lakshit-Chiranjiv <luckyhacker017@gmail.com>
2023-04-02 23:20:11 +0530

    search state and btn

--- app/page.js
+++ app/page.js
@@ -1,9 +1,14 @@
+'use client'
 import Image from 'next/image'
 import { Poppins } from 'next/font/google'
 import logo from '../public/knowitlogo.png'
 
+import { useState } from 'react'
+
 
 export default function Home() {
+
+  const [search, setSearch] = useState('')
 
   return (
     <main className='bg-slate-600 min-h-screen'>
@@ -17,6 +22,7 @@ export default function Home() {
         />
         <h1 className='text-2xl md:text-3xl lg:text-5xl text-white font-bold'>Search for any topic</h1>
         <input className='w-3/4 md:w-1/2 lg:w-1/3 h-10 rounded-md text-center outline-none' type='text' placeholder='Search' />
+        <button className='w-3/4 md:w-1/2 lg:w-1/3 h-10 rounded-md bg-slate-500 text-white font-bold active:scale-95'>Know</button>
       </div>
     </main>
   )
