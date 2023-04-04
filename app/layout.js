55040f5d58f1258894f4f0bc5105d90158812a28
Lakshit-Chiranjiv <luckyhacker017@gmail.com>
2023-04-04 14:28:19 +0530

    tried poppins font

--- app/layout.js
+++ app/layout.js
@@ -1,8 +1,15 @@
 import './globals.css'
 
+
+// include the poppins font from google fonts in the head using metadata api
 export const metadata = {
   title: 'Know It',
-  description: 'Provides Brief Information About Various Topics',
+  description: 'Know It is a web app that generates a summary for any topic',
+  font: {
+    google: {
+      families: ['Poppins']
+    }
+  }
 }
 
 export default function RootLayout({ children }) {
