a0e8bb8889efc9720a0eca937af2da08163add14
Lakshit-Chiranjiv <luckyhacker017@gmail.com>
2023-04-04 13:07:34 +0530

    corrected the errors on model code

--- app/page.js
+++ app/page.js
@@ -9,24 +9,24 @@ import { useState } from 'react'
 
 export default function Home() {
 
-  const openai = new OpenAI(process.env.OPENAI_API_KEY)
+  const openai = new OpenAI(process.env.NEXT_PUBLIC_OPENAI_API_KEY)
   const [search, setSearch] = useState('')
   const [summary, setSummary] = useState('')
 
   const generateSummary = async (topic) => {
     const result = await openai.complete({
-      engine: 'davinci',
-      prompt: `Generate a summary for ${topic}.`,
-      maxTokens: 50,
+      engine: 'text-davinci-003',
+      prompt: `Generate a 2 paragraph summary for ${topic}. Have atleast 120 words. \n\n`,
+      maxTokens: 100,
     });
-    const summary = result.data.choices[0].text;
-    setSummary(summary)
+    const res = result.data.choices[0].text;
+    setSummary(res)
   };
   
 
   return (
     <main className='bg-slate-600 min-h-screen'>
-      <div className='flex flex-col items-center justify-center gap-12 h-full'>
+      <div className='flex flex-col items-center justify-center gap-12 min-h-full'>
         <Image
           src={logo}
           alt='Know It Logo'
@@ -43,7 +43,7 @@ export default function Home() {
         }}>Know</button>
 
         {summary && 
-          <div className='w-3/4 md:w-1/2 lg:w-1/3 h-10 rounded-md bg-slate-500 text-white/90 font-semibold'>
+          <div className='w-3/4 md:w-1/2 lg:w-1/3 p-4 mb-12 rounded-md bg-slate-500 text-white/90 font-semibold'>
             <p>{summary}</p>
           </div>
         }
