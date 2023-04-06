4cab7f2fc138ecc3ec91026a4f27754b8e8137f3
Lakshit-Chiranjiv <luckyhacker017@gmail.com>
2023-04-06 21:03:06 +0530

    poppins font added

--- app/layout.js
+++ app/layout.js
@@ -1,13 +1,20 @@
 import './globals.css'
-
+import { Poppins } from '@next/font/google'
+ 
 export const metadata = {
   title: 'Know It',
   description: 'Know It is a web app that generates a summary for any topic',
 }
 
+const poppins = Poppins({
+  variable: '--display-poppins',
+  weight: ['100', '300', '400', '500', '700', '900'],
+  subsets: ['latin'],
+})
+
 export default function RootLayout({ children }) {
   return (
-    <html lang="en">
+    <html lang="en" className={`${poppins.variable}`}>
       <body>{children}</body>
     </html>
   )
