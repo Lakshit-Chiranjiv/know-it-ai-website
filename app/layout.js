d2582a646f3a90701422be7c0b5a6d68ca676c54
Lakshit-Chiranjiv <luckyhacker017@gmail.com>
2023-04-06 20:43:14 +0530

    removed font from metadata

--- app/layout.js
+++ app/layout.js
@@ -1,15 +1,8 @@
 import './globals.css'
 
-
-// include the poppins font from google fonts in the head using metadata api
 export const metadata = {
   title: 'Know It',
   description: 'Know It is a web app that generates a summary for any topic',
-  font: {
-    google: {
-      families: ['Poppins']
-    }
-  }
 }
 
 export default function RootLayout({ children }) {
